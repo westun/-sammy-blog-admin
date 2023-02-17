5d8ebca0fd0293cc42753d80d7a5eab9519abc6e
Wesley Tunnermann <wesleytunnermann2@hotmail.com>
2023-02-16 19:45:01 -0800

    first check in

---
+++ src/components/navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Sammy Blog Admin
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link active" aria-current="page">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/posts" className="nav-link">
+                Posts
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
