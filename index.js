3c038b44f023799608c8b6b1c64aa0116c3f98ee
Wesley Tunnermann <wesleytunnermann2@hotmail.com>
2023-02-20 00:56:03 -0800

    add node "mini" backend with express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const PORT = 8080;
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json(process.env.API_KEY); //not actually what to do, just for testing
+});
+
+app.get("/posts", (req, res) => {});
+
+app.listen(8080, () => console.log("server is running on port " + PORT));
