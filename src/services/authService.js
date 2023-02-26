25216a0dbe36e29b376cbde6a94fe68f5d0499d5
Wesley Tunnermann <wesleytunnermann2@hotmail.com>
2023-02-25 19:34:30 -0800

    Refactor: create authTokenStore

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,53 +1,36 @@
 import { getToken, getRefreshToken } from "./authTokenService";
-import * as AuthConstants from "../constants/authConstants";
+import * as authTokenService from "./authTokenService";
 import { isTokenExpired } from "../util/authToken";
+import * as authTokenStore from "../store/authTokenStore";
 
 export async function login(credentials) {
-  const result = await getToken(credentials);
+  const result = await authTokenService.getToken(credentials);
 
-  saveTokenToStore(result.data.token, result.data.refreshToken);
+  authTokenStore.saveToken(result.data.token, result.data.refreshToken);
 
   return result;
 }
 
 export function logout() {
-  removeTokenFromStore();
+  authTokenStore.removeToken();
 }
 
 export async function renewLogin() {
-  const tokenResult = getTokenFromStore();
+  const tokenResult = authTokenStore.getToken();
 
-  const { data: newToken } = await getRefreshToken(
+  const { data: newToken } = await authTokenService.getRefreshToken(
     tokenResult.token,
     tokenResult.refreshToken
   );
 
-  saveTokenToStore(newToken.token, newToken.refreshToken);
+  authTokenStore.saveToken(newToken.token, newToken.refreshToken);
 }
 
 export function isAuthenticated() {
-  var tokenResult = getTokenFromStore();
+  var tokenResult = authTokenStore.getToken();
   if (!tokenResult || !tokenResult.token) {
     return false;
   }
 
   return !isTokenExpired(tokenResult.token);
-}
-
-function getTokenFromStore() {
-  const token = localStorage.getItem(AuthConstants.LOCAL_STORAGE_TOKEN_KEY);
-  const refreshToken = localStorage.getItem(
-    AuthConstants.LOCAL_STORAGE_REFRESH_TOKEN
-  );
-  return { token, refreshToken };
-}
-
-function saveTokenToStore(token, refreshToken) {
-  localStorage.setItem(AuthConstants.LOCAL_STORAGE_TOKEN_KEY, token);
-  localStorage.setItem(AuthConstants.LOCAL_STORAGE_REFRESH_TOKEN, refreshToken);
-}
-
-function removeTokenFromStore() {
-  localStorage.removeItem(AuthConstants.LOCAL_STORAGE_TOKEN_KEY);
-  localStorage.removeItem(AuthConstants.LOCAL_STORAGE_REFRESH_TOKEN);
 }
---
+++ src/store/authTokenStore.js
@@ -0,0 +1,19 @@
+import * as AuthConstants from "../constants/authConstants";
+
+export function getToken() {
+  const token = localStorage.getItem(AuthConstants.LOCAL_STORAGE_TOKEN_KEY);
+  const refreshToken = localStorage.getItem(
+    AuthConstants.LOCAL_STORAGE_REFRESH_TOKEN
+  );
+  return { token, refreshToken };
+}
+
+export function saveToken(token, refreshToken) {
+  localStorage.setItem(AuthConstants.LOCAL_STORAGE_TOKEN_KEY, token);
+  localStorage.setItem(AuthConstants.LOCAL_STORAGE_REFRESH_TOKEN, refreshToken);
+}
+
+export function removeToken() {
+  localStorage.removeItem(AuthConstants.LOCAL_STORAGE_TOKEN_KEY);
+  localStorage.removeItem(AuthConstants.LOCAL_STORAGE_REFRESH_TOKEN);
+}
